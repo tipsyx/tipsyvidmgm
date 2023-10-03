55234378764827a0b56795a9c343ef76f611f10e
tipsyx <141881555+tipsyx@users.noreply.github.com>
2023-10-03 05:05:36 +0100

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,44 +1,6 @@
 package utils
 
 import (
-    "github.com/gerow/thumbs"
     "io"
     "net/http"
 )
-
-func GenerateVideoThumbnail(videoPath string, thumbnailPath string) error {
-    config := thumbs.Config{
-        SeekPercentage: 10,
-        Width:          320,
-        Height:         240,
-        Output:         thumbnailPath,
-    }
-
-    err := thumbs.Create(videoPath, config)
-    if err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func validateScreenRecording(file io.Reader, filename string) bool {
-    ext := filepath.Ext(filename)
-    switch ext {
-    case ".mp4", ".avi", ".mov", ".mkv", ".webm", ".flv":
-        return true
-    } 
-    
-    buffer := make([]byte, 512)
-    _, err := file.Read(buffer)
-    if err != nil && err != io.EOF {
-        return false
-    }
-    mimeType := http.DetectContentType(buffer)
-    switch mimeType {
-    case "video/mp4", "video/avi", "video/webm", "video/x-msvideo", "video/flv", "video/quicktime", "video/x-matroska":
-        return true
-    default:
-        return false
-    }
-}

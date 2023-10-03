0b50d60061416925ddd29a523e04ae0ff0fd09de
tipsyx <141881555+tipsyx@users.noreply.github.com>
2023-10-03 05:20:43 +0100

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,6 +1,6 @@
 package config
 
 const (
-    DatabaseDSN = "Tipsyx:#MarquessROOT28@tcp(127.0.0.1:3306)/videomanagement?charset=utf8&parseTime=True&loc=Local"
+    DatabaseDSN = ""
     RabbitMQURL = ""
 )
